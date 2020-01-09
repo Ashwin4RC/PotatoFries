f8c51173b0a134e63c2e05fc7c4da3abe18f2310
Ashwin Prakash R C <ashwin2001achu@gmail.com>
2020-01-09 08:55:12 +0530

    add aod on charge
    -Kanged from ezio

--- lib/data/lock_screen.dart
+++ lib/data/lock_screen.dart
@@ -1,1 +1,19 @@
-final Map<String, dynamic> lockScreen = {};
+import 'package:android_flutter_settings/android_flutter_settings.dart';
+import 'package:flutter/material.dart';
+import 'package:potato_fries/widgets/directory.dart';
+
+final Map<String, dynamic> lockScreen = {
+  'Always-On-Display': aod,
+};
+
+final Map<String, dynamic> aod = {
+  'doze_on_charge': {
+    'title': 'Always on Charging',
+    'subtitle': 'Show Always On Ambient display when charging',
+    'widget': WidgetType.SWITCH,
+    'setting_type': SettingType.SYSTEM,
+    'widget_data': {
+      'default': false,
+    }
+  },
+};
